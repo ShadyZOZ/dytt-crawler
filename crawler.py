b8e3b7c03e0269e19199afa5babb8bd780877e26
shadyzoz <shadyzoz@163.com>
2016-08-16 02:18:30 +0800

    added add to watch list function

--- crawler.py
+++ crawler.py
@@ -8,15 +8,32 @@ from datetime import date
 
 from bs4 import BeautifulSoup
 
-use_db = True
 
-try:
-    from pymongo import MongoClient
-    print('pymongo found, running on db mode!')
-except ImportError as e:
-    use_db = False
-    print('import error:', e)
-    print('running on no db mode!')
+def get_db():
+    try:
+        from pymongo import MongoClient
+        print('pymongo found, running on db mode!')
+        client = MongoClient()
+        db = client.dytt
+        return True, db
+    except ImportError as e:
+        print('import error:', e)
+        print('running on no db mode!')
+        return False, None
+
+def add_to_watch_list():
+    use_db_flag = False
+    while True:
+        option = input('Add to watch list (y/n)? ')
+        if option == 'y':
+            use_db_flag = True
+            break
+        elif option == 'n':
+            break
+        else:
+            print('unrecognized option!')
+    if use_db_flag:
+        return get_db()
 
 def get_results(keyword):
     keyword_bytes = bytes(keyword, 'gbk')
@@ -35,7 +52,7 @@ def get_results(keyword):
         print('AttributeError: ', e)
         return None
 
-def get_download_list(name, url):
+def save_download_list(name, url, flag, db):
     try:
         html = urlopen(url)
     except (HTTPError, URLError) as e:
@@ -50,41 +67,66 @@ def get_download_list(name, url):
             os.mkdir(file_path)
         file_name = os.path.join(file_path, name)
         with open(file_name + '.txt', 'w') as f:
+            if flag:
+                document = {
+                    'name': name,
+                    'url': url,
+                    'file_path': file_name,
+                    'urls': [],
+                    'latest': len(td_tags),
+                    'update_on': date.today(),
+                    'ended': False
+                }
             f.write(str(date.today()) + '\n')
             for td_tag in td_tags:
                 a_tag = td_tag.a
-                f.write(a_tag.getText() + '\n')
+                text = a_tag.getText()
+                f.write(text + '\n')
+                if flag:
+                    document['urls'].append(text)
+            if flag:
+                from pymongo import errors
+                try:
+                    db.watchlist.insert_one(document)
+                    db.watchlist.create_index('url', unique=True)
+                except errors.OperationFailure as e:
+                    print('db error', e)
         return True
     except AttributeError as e:
         print('AttributeError: ', e)
         return None
 
+def get_or_create_obj():
+    pass
+
 def main():
     keyword = input('TVs/Movies to search: ')
     a_tags = get_results(keyword)
-    if a_tags == None:
+    if a_tags is None:
         print('No results found')
+        print('exit program')
     else:
         print('0 | get all')
         for a_tag in a_tags:
             print(a_tags.index(a_tag) + 1, '|', re.sub(r'<font color="red">|</font>', r'', a_tag.getText()), ': http://www.ygdy8.com%s' % a_tag['href'])
-    print(len(a_tags) + 1, '| exit')
-    choice = input('choose result(s) to get download links: ')
-    choice = choice.split(' ')
-    url_list = []
-    if choice[0] == '0':
-        url_list = a_tags
-    elif str(len(a_tags) + 1) in choice:
-        return
-    else:
-        for i in choice:
-            url_list.append(a_tags[int(i) - 1])
-    for url_info in url_list:
-        result = get_download_list(url_info.getText(), 'http://www.ygdy8.com' + url_info['href'])
-        if result:
-            print('require', url_info.getText(), 'success')
+        print(len(a_tags) + 1, '| exit')
+        choice = input('choose result(s) to get download links: ')
+        choice = choice.split(' ')
+        url_list = []
+        if choice[0] == '0':
+            url_list = a_tags
+        elif str(len(a_tags) + 1) in choice:
+            return
         else:
-            print('require', url_info.getText(), 'failed')
+            for i in choice:
+                url_list.append(a_tags[int(i) - 1])
+        flag, db = add_to_watch_list()
+        for url_info in url_list:
+            result = save_download_list(url_info.getText(), 'http://www.ygdy8.com' + url_info['href'], flag, db)
+            if result:
+                print('require', url_info.getText(), 'success')
+            else:
+                print('require', url_info.getText(), 'failed')
 
 if __name__ == '__main__':
     main()
